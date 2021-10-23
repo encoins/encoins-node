6160b614c503198d65d5bd3459d296a3970b93f3
Arthur Vinciguerra <558-avincigu@users.noreply.gitlab.aliens-lyon.fr>
2021-10-23 16:46:27 +0200

    +transaction struct

--- encoins/src/main.rs
+++ encoins/src/main.rs
@@ -1,26 +1,10 @@
+mod transaction;
 extern crate mpi;
 
-use mpi::traits::*;
+//use mpi::traits::*;
 
 fn main() {
-    let universe = mpi::initialize().unwrap();
-    let world = universe.world();
-    let size = world.size();
-    let rank = world.rank();
+    let tr = transaction::Transaction{seq_id: 1, sender_id: 0, receiver_id: 0, amount: 0 };
+    transaction::print_transaction(&tr);
 
-    if size != 2 {
-        panic!("Size of MPI_COMM_WORLD must be 2, but is {}!", size);
-     }
-
-    match rank {
-        0 => {
-            let msg = vec![4.0f64, 8.0, 15.0];
-            world.process_at_rank(rank + 1).send(&msg[..]);
-        }
-        1 => {
-            let (msg, status) = world.any_process().receive_vec::<f64>();
-            println!("Process {} got message {:?}.\nStatus is: {:?}", rank, msg, status);
-        }
-        _ => unreachable!()
-    }
 }
---
+++ encoins/src/transaction.rs
@@ -0,0 +1,24 @@
+/// We define aliases here to be able to change our mind on the implementation in the future
+pub type UserId = i32;
+pub type Currency = i32;
+pub type SeqId = i32;
+
+/// Defining a base struct transaction
+pub struct Transaction
+{
+    /// seq_id is the id of the transaction. For a transaction t, seq_id will be the number of validated transfers outgoing form the sender +1.
+    pub(crate) seq_id: SeqId,
+    pub(crate) sender_id: UserId,
+    pub(crate) receiver_id: UserId,
+    pub(crate) amount: Currency
+}
+
+pub fn print_transaction(transaction: &Transaction)
+{
+    println!("Transaction infos:     \n\
+             \t- Sender Id : {}       \n\
+             \t- Receiver Id : {}      \n\
+             \t- Sender's seq id : {} \n\
+             \t- Amount transferred : {}\n"
+             , transaction.sender_id, transaction.receiver_id, transaction.seq_id, transaction.amount)
+}
