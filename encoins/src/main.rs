a6f38c8d392231d9c640b6725cca3845f8857d2e
= <578-sbounan@users.noreply.gitlab.aliens-lyon.fr>
2021-11-14 19:17:44 +0100

    new types + fonction save_transaction

--- encoins/src/main.rs
+++ encoins/src/main.rs
@@ -5,6 +5,7 @@ extern crate mpi;
 use mpi::request::WaitGuard;
 use mpi::traits::*;
 
+const M: usize = 10;
 
 fn main() {
     // An example of a transaction
@@ -12,22 +13,28 @@ fn main() {
     let world = universe.world();
     let size = world.size();
     let rank = world.rank();
-    let p = processus::Processus::init(rank);
+    let mu:[usize;M] = [0;M];
+    let mut p = processus::Processus::init(rank, mu);
+    let root_rank = 0;
 
+    p.transfert(0,1,0);
+
+    
+    
+    
     /* 
     Thread 1 :
         loop:
             if p.toValidate not empty:
                 (q, t, h) = toValidate.pop()
                 if p.valid(q, t, h):
-                    p.adjust(q, t, h)
+                    p.adjust(q, t, h) (l.14-20)
     
     Thread 2 :
         if rank == 0:
             p.transfert(0, 1, 0)
         loop:
             Rcv(q,m)
-            p.verify(q,m)
+            p.verify(q,m) (l.9-12)
     */
-
 }
--- encoins/src/processus.rs
+++ encoins/src/processus.rs
@@ -3,55 +3,68 @@ use crate::transaction::{UserId, Currency, Transaction};
 
 
 const N : usize = 10;
-type List = [u32;N];
-type Set = Vec<HashSet<Transaction>>;
+const M : usize = 10;
+type ProcId = usize;
+type Table = [u32;N];
+type Stack = Vec<Transaction>;
 
 
 pub struct Processus {
-    id_proc : UserId,
-    seq : List,
-    rec : List,
-    hist : Set,
-    deps : HashSet<Transaction>,
-    to_validate : HashSet<Transaction>
+    proc_id : UserId,
+    seq : Table,
+    rec : Table,
+    hist : Vec<Stack>,
+    deps : Stack,
+    to_validate : Stack,
+    mu : [ProcId;M]
 }
 
 
 impl Processus {
-    pub fn init(rank: i32) -> Processus {
-        let mut s= vec![];
+    pub fn init(rank: i32, mu: [ProcId;M]) -> Processus {
+        let mut hist = vec![];
         for i in 1..N {
-            s.push(HashSet::<Transaction>::new())
+            let new_el:Stack = vec![];
+            hist.push(new_el);
+
         }
         Processus {
-            id_proc : rank as u32,
+            proc_id : rank as u32,
             seq : [0;N],
             rec : [0;N],
-            hist : s,
-            deps : HashSet::<Transaction>::new(),
-            to_validate : HashSet::<Transaction>::new()
+            hist : hist,
+            deps : vec![],
+            to_validate : vec![],
+            mu : mu
         }
     }
     
-    fn transfert(& mut self, user_id: UserId, receiver_id: UserId, amount : Currency) -> bool {
-        if self.read() < amount {
-            return false
+    pub fn transfert(& mut self, user_id: UserId, receiver_id: UserId, amount : Currency) -> () {
+        if self.read(user_id) < amount {
+            println!("Not enough money");
         }
-        // let message = (sender_id, receiverId, amount, &self.seq[self.id_proc] + 1, &self.deps );
+        else {
+            self.deps = vec![];
+        }
+        // let message = (sender_id, receiverId, amount, &self.seq[self.proc_id] + 1, &self.deps );
         // message.sign() : Waiting for Milan
         // broadcast(message); Waiting for Arthur
-        self.deps = HashSet::new();
-        true
+    }
+    fn save_transaction(& mut self, q:ProcId, t:Transaction) -> () {
+        if t.seq_id == self.rec[q] + 1 {
+            self.rec[q] += 1;
+            self.to_validate.push(t);
+        }
+        else {
+            println!("Some transactions of proc {} have not been received by proc {}", q, self.proc_id);
+        }
     }
 
-    fn read(&self) -> u32 {
-        let a = self.id_proc;
-        let dep = &self.hist[a as usize];
-        // dep = dep.union(&deps)
-        return Processus::balance(a, dep)
+    fn read(&self, a:UserId) -> Currency {
+        Processus::balance(a, &self.hist[a as usize]) + Processus::balance(a, &self.deps)
     }
 
-    fn balance(a: UserId, h: &HashSet<Transaction>) -> Currency {
+    fn balance(a: UserId, h: &Stack) -> Currency {
         let mut balance : u32 = 0;
         for transfert in h {
             if transfert.receiver_id == a {
