b042d5342bcca51e6d46e07f463591c415dd3e10
= <578-sbounan@users.noreply.gitlab.aliens-lyon.fr>
2021-11-14 15:02:39 +0100

    update of the basis

--- encoins/src/main.rs
+++ encoins/src/main.rs
@@ -5,9 +5,9 @@ extern crate mpi;
 use mpi::request::WaitGuard;
 use mpi::traits::*;
 
-//use mpi::traits::*;
 
 fn main() {
+    // An example of a transaction
     let tr = transaction::Transaction{seq_id: 1, sender_id: 0, receiver_id: 0, amount: 0 };
     transaction::print_transaction(&tr);
     let universe = mpi::initialize().unwrap();
