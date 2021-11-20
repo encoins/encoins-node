0999ad0ff3a5de0ba2b101d527e5dc5f3634d126
Guillaume Méroué <guillaume.meroue@ens-lyon.fr>
2021-11-21 00:04:00 +0100

    Implementation of operations Transfers and Read ( + removal of t from transfert )

--- encoins/src/processus.rs
+++ encoins/src/processus.rs
@@ -3,19 +3,21 @@
 use crate::transaction::Transaction;
 use crate::base_types::*;
 use std::sync::mpsc::{Receiver, Sender};
-use crate::message::Message;
+use crate::message::{Message, STANDARD};
+use crate::messaging::broadcast;
 
 type List = Vec<u32>;
-type TransfertSet = Vec<Transaction>;
+type TransferSet = Vec<Transaction>;
+
 
 
 pub struct Processus {
     id_proc : UserId,
     seq : List,
     rec : List,
-    hist : Vec<TransfertSet>,
-    deps : TransfertSet,
-    to_validate : TransfertSet,
+    hist : Vec<TransferSet>,
+    deps : TransferSet,
+    to_validate : TransferSet,
     senders : Vec<Sender<Message>>,
     receiver : Receiver<Message>
 }
@@ -23,47 +25,59 @@ pub struct Processus {
 
 impl Processus {
     pub fn init(id : UserId, nb_process : u32, senders : Vec<Sender<Message>>, receiver : Receiver<Message>) -> Processus {
-        let mut s : Vec<TransfertSet> = vec![];
+        let mut s : Vec<TransferSet> = vec![];
         for i in 0..nb_process {
-            s.push(TransfertSet::new())
+            s.push(TransferSet::new())
         }
         Processus {
             id_proc : id,
             seq : vec![0;nb_process as usize],
             rec : vec![0;nb_process as usize],
             hist : s,
-            deps : TransfertSet::new(),
-            to_validate : TransfertSet::new(),
+            deps : TransferSet::new(),
+            to_validate : TransferSet::new(),
             senders,
             receiver
         }
     }
 
-    pub fn transfert(& mut self, user_id: UserId, receiver_id: UserId, amount : Currency) -> bool {
+    pub fn transfer(& mut self, user_id: UserId, receiver_id: UserId, amount : Currency) -> bool {
         if self.read() < amount {
             return false
         }
-        // let message = (sender_id, receiverId, amount, &self.seq[self.id_proc] + 1, &self.deps );
+
+        let message : Message = Message {
+            transaction : Transaction {
+                seq_id : self.seq[receiver_id as usize] + 1,
+                sender_id : user_id,
+                receiver_id,
+                amount,
+            },
+            dependencies : self.deps.clone(),
+            message_type : STANDARD,
+            signature : 0 // we all count on Milan
+
+        };
         // message.sign() : Waiting for Milan
-        // broadcast(message); Waiting for Arthur
-        self.deps = TransfertSet::new();
+        broadcast(&self.senders, message);
+        self.deps = TransferSet::new();
         true
     }
 
-    fn read(&self) -> Currency {
+    pub fn read(&self) -> Currency {
         let a = self.id_proc;
-        let dep = &self.hist[a as usize];
-        // dep = dep.union(&deps)
-        return Processus::balance(a, dep)
+        let mut dep = self.hist[a as usize].clone();
+        dep.append(&mut self.deps.clone());
+        return Processus::balance(a, &dep)
     }
 
-    fn balance( a: UserId, h: &TransfertSet) -> Currency {
+    fn balance( a: UserId, h: &TransferSet) -> Currency {
         let mut balance : u32 = 0;
-        for transfert in h {
-            if transfert.receiver_id == a {
-                balance += transfert.amount;
+        for transfer in h {
+            if transfer.receiver_id == a {
+                balance += transfer.amount;
             } else {
-                balance -= transfert.amount;
+                balance -= transfer.amount;
             }
         }
         balance
