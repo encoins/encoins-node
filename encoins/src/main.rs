0d52fc9ca4d756e9e1efb12dbcf72ff130a0b7d5
Arthur Vinciguerra <558-avincigu@users.noreply.gitlab.aliens-lyon.fr>
2021-11-20 18:32:25 +0100

    - Logging function implemented

--- encoins/src/main.rs
+++ encoins/src/main.rs
@@ -17,12 +17,16 @@ fn main()
 {
     // Gets given arguments at execution
     let args: Vec<String> = env::args().collect();
+    let number_of_processes = args[1].parse::<u32>().unwrap();
+
+    // Initialize logging
+    logging::initialize(number_of_processes);
 
     // Creating transmitter and receiver for main
     let (transmit_main, receive_main): (Sender<Message>, Receiver<Message>) = mpsc::channel();
 
-    &println!("Initializing with {} processes", &args[1]);
-    let senders= initialize_processes(args[1].parse::<u32>().unwrap(), &transmit_main, &receive_main);
+    println!("Initializing with {} processes", &args[1]);
+    let senders= initialize_processes(number_of_processes, &transmit_main, &receive_main);
     thread::sleep(Duration::from_millis(1000));
 
 
@@ -36,17 +40,17 @@ fn initialize_processes(nb_process: u32, main_transmitter: &Sender<Message>, mai
     for i in 0..nb_process {
         let main_transmitter = main_transmitter.clone();
 
-        let thread_senders = senders.clone();
+        /*let thread_senders = senders.clone();
         let thread_receiver = match receivers.get(i as usize) {
             None => panic!("Something went wrong in the initialization!"),
             Some(x) => x.clone(),
-        };
+        };*/
 
         thread::spawn(move || {
             let proc_nb = i+1;
-
+            log!(proc_nb, "Thread initialized correctly");
             loop {
-                messaging::deal_with_messages(proc_nb,thread_receiver, &thread_senders, &main_transmitter);
+               // messaging::deal_with_messages(proc_nb,thread_receiver, &thread_senders, &main_transmitter);
                 thread::sleep(Duration::from_millis(500));
             }
         });
