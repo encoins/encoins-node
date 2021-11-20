a9cdea371b379ac3a7483894bbb6883687aa15b1
Arthur Vinciguerra <558-avincigu@users.noreply.gitlab.aliens-lyon.fr>
2021-11-21 00:08:33 +0100

    - Minor graphical enhancements

--- encoins/src/input_management.rs
+++ encoins/src/input_management.rs
@@ -8,6 +8,7 @@
 use std::io;
 use std::io::Write;
 use std::process::Command;
+use std::sync::mpsc::SyncSender;
 use crate::communication::Communication;
 use crate::transaction::Transaction;
 
@@ -15,67 +16,67 @@ use crate::transaction::Transaction;
 pub fn read_input() -> Option<Communication>{
     
     // Parameters
-    let nb_args_required: [usize; 6] = [3, 3, 4, 2,0,0];
+    let nb_args_required: [usize; 7] = [3, 3, 4, 2, 1 ,1, 1];
 
-    loop {
-        show_terminal();
+    show_terminal();
 
-        // Save the line entered on the terminal in the string input_line
-        let mut input_line = String::new();
+    // Save the line entered on the terminal in the string input_line
+    let mut input_line = String::new();
 
-        io::stdin()
-            .read_line(&mut input_line)
-            .expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut input_line)
+        .expect("Failed to read line");
 
-        // Deletion of the last character : '\n'
-        let len = input_line.len();
-        let input_line = &input_line[..len-1];
+    // Deletion of the last character : '\n'
+    let len = input_line.len();
+    let input_line = &input_line[..len-1];
 
-        // Parsing of the input line as an op_type and an array args of arguments, managing the syntax errors
-        let words: Vec<&str> = input_line.split(' ').collect();
-        // op_type
-        let mut op_type: usize = match words[0] {
-            "add"       => 0,
-            "remove"    => 1,
-            "transfer" => 2,
-            "read"      => 3,
-            "help"      => 4,
-            "clear"     => 5,
-            _           => {
-                println!("Unknown operation");
-                continue
-            }
-        };
-
-        if words.len() != nb_args_required[op_type] {
-            println!("Wrong number of arguments");
-
+    // Parsing of the input line as an op_type and an array args of arguments, managing the syntax errors
+    let words: Vec<&str> = input_line.split(' ').collect();
+    // op_type
+    let mut op_type: usize = match words[0] {
+        "add"       => 0,
+        "remove"    => 1,
+        "transfer" => 2,
+        "read"      => 3,
+        "help"      => 4,
+        "clear"     => 5,
+        "quit"      => 6,
+        _           => {
+            println!("Unknown operation");
+            7000
         }
-        else
-        {
+    };
 
-            // args
-            let mut args: Vec<u32> = vec![];
-            for k in 1..nb_args_required[op_type] {
-                let word = String::from(words[k]);
-                let arg: u32 = match word.trim().parse() {
-                    Ok(num) => num,
-                    Err(_) => {
-                        println!("Please type numbers as arguments");
-                        continue
-                    }
-                };
-                args.push(arg);
-            }
-
-            // Returning the corresponding transaction
-
-        }
-
+    if op_type>6 || words.len() != nb_args_required[op_type] {
+        println!("Wrong number of arguments");
+        None
     }
+    else
+    {
+
+        // args
+        let mut args: Vec<u32> = vec![];
+        for k in 1..nb_args_required[op_type] {
+            let word = String::from(words[k]);
+            let arg: u32 = match word.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Please type numbers as arguments");
+                    continue
+                }
+            };
+            args.push(arg);
+        }
+
+        // Returning the corresponding transaction
+        deal_with_entry(args, op_type)
+    }
+
+
 }
 
-fn deal_with_entry(args : Vec<u32>, op_type : u32) -> Option<Communication>
+fn deal_with_entry(args : Vec<u32>, op_type : usize) -> Option<Communication>
 {
 
     match op_type {
@@ -109,7 +110,13 @@ fn deal_with_entry(args : Vec<u32>, op_type : u32) -> Option<Communication>
 
         5 =>
             {
+                Command::new("clear").spawn().expect("error");
                 None
+            }
+        6 =>
+            {
+                println!("Goodbye!");
+                std::process::exit(0);
             }
         _ => {
             panic!("ALALALA");
@@ -126,21 +133,47 @@ fn show_terminal()
 
 fn show_help()
 {
-    Command::new("clear").spawn().expect("error");
     print_logo();
-    println!("============================================================================================================================\n\n
-            Available commands : \n
-            \t• add <account> <amount>                  : Adds <amount> of coins to the account <account>\n
-            \t• remove <account> <amount>               : Removes <amount> of coins from the account <account>\n
-            \t• transfer <account1> <account2> <amount> : Transfers <amount> of coins from account <account1> to account <account2> \n
-            \t• read <account>                          : Displays the current amount of money of the account <account>\n
-            \t• clear                                   : Clears terminal from previous entered instructions \n
-            \t• help                                    : Displays the list of possible instructions \n
-            \t• q                                       : To exit this window\n
-            ============================================================================================================================\n\n");
+    println!(
+        "\n\n\
+        ============================================================================================================================\n\n\
+        Available commands : \n
+        \t• add <account> <amount>                  : Adds <amount> of coins to the account <account>\n
+        \t• remove <account> <amount>               : Removes <amount> of coins from the account <account>\n
+        \t• transfer <account1> <account2> <amount> : Transfers <amount> of coins from account <account1> to account <account2> \n
+        \t• read <account>                          : Displays the current amount of money of the account <account>\n
+        \t• clear                                   : Clears terminal from previous entered instructions \n
+        \t• help                                    : Displays the list of possible instructions \n
+        \t• quit                                    : Quits program\n
+        \n============================================================================================================================\n\n");
 }
 
 fn print_logo()
 {
-    println!()
+    println!("=================================================================================================================================================================================
+          _____                    _____                    _____                   _______                   _____                    _____                    _____
+         /\\    \\                  /\\    \\                  /\\    \\                 /::\\    \\                 /\\    \\                  /\\    \\                  /\\    \\
+        /::\\    \\                /::\\____\\                /::\\    \\               /::::\\    \\               /::\\    \\                /::\\____\\                /::\\    \\
+       /::::\\    \\              /::::|   |               /::::\\    \\             /::::::\\    \\              \\:::\\    \\              /::::|   |               /::::\\    \\
+      /::::::\\    \\            /:::::|   |              /::::::\\    \\           /::::::::\\    \\              \\:::\\    \\            /:::::|   |              /::::::\\    \\
+     /:::/\\:::\\    \\          /::::::|   |             /:::/\\:::\\    \\         /:::/~~\\:::\\    \\              \\:::\\    \\          /::::::|   |             /:::/\\:::\\    \\
+    /:::/__\\:::\\    \\        /:::/|::|   |            /:::/  \\:::\\    \\       /:::/    \\:::\\    \\              \\:::\\    \\        /:::/|::|   |            /:::/__\\:::\\    \\
+   /::::\\   \\:::\\    \\      /:::/ |::|   |           /:::/    \\:::\\    \\     /:::/    / \\:::\\    \\             /::::\\    \\      /:::/ |::|   |            \\:::\\   \\:::\\    \\
+  /::::::\\   \\:::\\    \\    /:::/  |::|   | _____    /:::/    / \\:::\\    \\   /:::/____/   \\:::\\____\\   ____    /::::::\\    \\    /:::/  |::|   | _____    ___\\:::\\   \\:::\\    \\
+ /:::/\\:::\\   \\:::\\    \\  /:::/   |::|   |/\\    \\  /:::/    /   \\:::\\    \\ |:::|    |     |:::|    | /\\   \\  /:::/\\:::\\    \\  /:::/   |::|   |/\\    \\  /\\   \\:::\\   \\:::\\    \\
+/:::/__\\:::\\   \\:::\\____\\/:: /    |::|   /::\\____\\/:::/____/     \\:::\\____\\|:::|____|     |:::|    |/::\\   \\/:::/  \\:::\\____\\/:: /    |::|   /::\\____\\/::\\   \\:::\\   \\:::\\____\\
+\\:::\\   \\:::\\   \\::/    /\\::/    /|::|  /:::/    /\\:::\\    \\      \\::/    / \\:::\\    \\   /:::/    / \\:::\\  /:::/    \\::/    /\\::/    /|::|  /:::/    /\\:::\\   \\:::\\   \\::/    /
+ \\:::\\   \\:::\\   \\/____/  \\/____/ |::| /:::/    /  \\:::\\    \\      \\/____/   \\:::\\    \\ /:::/    /   \\:::\\/:::/    / \\/____/  \\/____/ |::| /:::/    /  \\:::\\   \\:::\\   \\/____/
+  \\:::\\   \\:::\\    \\              |::|/:::/    /    \\:::\\    \\                \\:::\\    /:::/    /     \\::::::/    /                   |::|/:::/    /    \\:::\\   \\:::\\    \\
+   \\:::\\   \\:::\\____\\             |::::::/    /      \\:::\\    \\                \\:::\\__/:::/    /       \\::::/____/                    |::::::/    /      \\:::\\   \\:::\\____\\
+    \\:::\\   \\::/    /             |:::::/    /        \\:::\\    \\                \\::::::::/    /         \\:::\\    \\                    |:::::/    /        \\:::\\  /:::/    /
+     \\:::\\   \\/____/              |::::/    /          \\:::\\    \\                \\::::::/    /           \\:::\\    \\                   |::::/    /          \\:::\\/:::/    /
+      \\:::\\    \\                  /:::/    /            \\:::\\    \\                \\::::/    /             \\:::\\    \\                  /:::/    /            \\::::::/    /
+       \\:::\\____\\                /:::/    /              \\:::\\____\\                \\::/____/               \\:::\\____\\                /:::/    /              \\::::/    /
+        \\::/    /                \\::/    /                \\::/    /                 ~~                      \\::/    /                \\::/    /                \\::/    /
+         \\/____/                  \\/____/                  \\/____/                                           \\/____/                  \\/____/                  \\/____/\
+
+         \n\
+         =================================================================================================================================================================================");
 }
+
