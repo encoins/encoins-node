e2ecf3785edcd4b91afd3d9d246938962f04cb81
samouly <samuel.bounan@ens-lyon.fr>
2021-11-28 22:46:18 +0100

    adding atualization of messages in brb

--- encoins/src/messaging.rs
+++ encoins/src/messaging.rs
@@ -69,21 +69,57 @@ pub(crate) fn deal_with_comm(process: &mut Processus, comm: Communication)
     }
 }
 
+// A function that enters a byzantine reliable broadcast with the first message received
+// If everything goes well, pushed the final message in proc.to_validate
+// Else do not terminate
 fn brb(process: &mut Processus, init_msg: Message)
 {
+    // Initialization
     let nb_process = process.get_senders().len() as usize;
-    let echos: Vec<Option<Message>> = vec![None;nb_process];
-    let ready: Vec<Option<Message>> = vec![None;nb_process];
-    let actu_msg: Message = init_msg;
+    let mut echos: Vec<Option<Message>> = vec![None;nb_process];
+    let mut ready: Vec<Option<Message>> = vec![None;nb_process];
+    let mut actu_msg: Message = init_msg.clone();
     
+    // While not enough processes are ready
     while !quorum(&ready, (2*nb_process)/3, &actu_msg)
     {
+        // Create a new message ready to be sent/saved
+        let mut my_msg = actu_msg.clone();
+        my_msg.sender_id = process.get_id();
+        match actu_msg.message_type
+        {
+            MessageType::Init => 
+                {
+                    my_msg.message_type = MessageType::Echo;
+                    broadcast(&process.get_senders(), Communication::Transfer { message: my_msg });
+                }
+            
+            MessageType::Echo =>
+                {
+                    echos[actu_msg.sender_id as usize] = Some(actu_msg.clone());
+                }
+            
+            MessageType::Ready =>
+                {
+                    ready[actu_msg.sender_id as usize] = Some(actu_msg.clone());
+                }
+        }
 
+        // Actualize the actual message
+        let comm = process.get_receiver().recv().unwrap();
+        match comm
+        {
+            Communication::Transfer { message } => {actu_msg = message;}
+            _ => {panic!("During the byzantine reliable broadcast, a communication received is not a transfer");}
+        }
     }
 
+    // Save the message
     process.in_to_validate(actu_msg);
 }
 
+
+// Returns if the number of occurences of msg in tab is greater than k
 fn quorum(tab: &Vec<Option<Message>>, k: usize, msg: &Message) -> bool 
 {
     true
